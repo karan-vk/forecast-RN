2fb77ca3b74baeebaf66947a266e373f26c59dd4
Karan <karanvknarayanan@gmail.com>
2020-11-08 14:28:38 +0530

    picture taking

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,12 +1,69 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, Button, StyleSheet, Image } from "react-native";
+import { Camera } from "expo-camera";
 
-export default class Add extends Component {
-  render() {
-    return (
-      <View>
-        <Text> ADD </Text>
-      </View>
-    );
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      // console.log(data.uri);
+
+      setImage(data.uri);
+      console.log(image);
+    }
+  };
+  if (hasPermission === null) {
+    return <View />;
   }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={styles.cameraContainer}>
+        <Camera
+          ref={(ref) => setCamera(ref)}
+          style={styles.fixedRatio}
+          type={type}
+          ratio="1:1"
+        ></Camera>
+      </View>
+      <Button
+        title="flip"
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}
+      />
+      <Button title="take picture" onPress={() => takePicture()} />
+      {image ? <Text>{image}</Text> : null}
+      {image ? <Image source={{ uri: image }} style={{ flex: 1 }} /> : null}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  cameraContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1,
+  },
+});
